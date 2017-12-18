61659f58fdaa4e243527e1415de4b139bf6f0238
WEIZIBIN <1999597@qq.com>
2017-12-18 20:10:24 +0800

    upload project

---
+++ redis-demo/src/main/java/github/weizibin/IndexController.java
@@ -0,0 +1,19 @@
+package github.weizibin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/web/index")
+	public String webindex(){
+		return "index";
+	}
+	
+	@RequestMapping("/error/access_denied")
+	public String access_denied(){
+		return "common/access_denied";
+	}
+	
+}
