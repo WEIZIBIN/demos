385f71ff26d3462bc44f08699b94708e7b59196e
WEIZIBIN <1999597@qq.com>
2017-12-24 19:56:05 +0800

    finish dubbo-demo

---
+++ dubbo-demo/dubbo-consumer/src/main/java/github/weizibin/Application.java
@@ -0,0 +1,16 @@
+package github.weizibin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:application-dubbo.xml")
+public class Application  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
