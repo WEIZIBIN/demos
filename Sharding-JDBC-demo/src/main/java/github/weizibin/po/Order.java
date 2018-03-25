e99986e2a3141b81f1bc80794bd94f9635763f5e
WEIZIBIN <1999597@qq.com>
2018-03-26 00:07:04 +0800

    init Sharding-JDBC-demo

---
+++ Sharding-JDBC-demo/src/main/java/github/weizibin/po/Order.java
@@ -0,0 +1,35 @@
+package github.weizibin.po;
+
+import java.util.Date;
+
+public class Order {
+    private Integer id;
+
+    private String orderId;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ Sharding-JDBC-demo/src/test/java/github/weizibin/mapper/OrderMapperTest.java
@@ -0,0 +1,38 @@
+package github.weizibin.mapper;
+
+import github.weizibin.Application;
+import github.weizibin.po.Order;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import(Application.class)
+public class OrderMapperTest {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @org.junit.Test
+    public void insert() throws Exception {
+        Order order = new Order();
+        Date date = new Date();
+        order.setOrderId("" + date.getTime());
+        order.setCreateTime(date);
+        int insert = orderMapper.insert(order);
+        assert insert == 1;
+    }
+
+    @org.junit.Test
+    public void selectByPrimaryKey() throws Exception {
+        orderMapper.selectByPrimaryKey(1);
+    }
+
+}
