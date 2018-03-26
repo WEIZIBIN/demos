f99486501cf41f134f88da3deee6b6998b946fcd
WEIZIBIN <1999597@qq.com>
2018-03-26 18:40:52 +0800

    add sharding jar&alogorithm

--- Sharding-JDBC-demo/src/main/java/github/weizibin/Application.java
+++ Sharding-JDBC-demo/src/main/java/github/weizibin/Application.java
@@ -2,10 +2,12 @@ package github.weizibin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
 @ImportResource("classpath:applicationContext.xml")
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class Application {
 
     public static void main(String[] args) {
---
+++ Sharding-JDBC-demo/src/main/java/github/weizibin/algorithm/PreciseModuloTableShardingAlgorithm.java
@@ -0,0 +1,19 @@
+package github.weizibin.algorithm;
+
+import io.shardingjdbc.core.api.algorithm.sharding.PreciseShardingValue;
+import io.shardingjdbc.core.api.algorithm.sharding.standard.PreciseShardingAlgorithm;
+
+import java.util.Collection;
+
+public final class PreciseModuloTableShardingAlgorithm implements PreciseShardingAlgorithm<Integer> {
+    
+    @Override
+    public String doSharding(final Collection<String> availableTargetNames, final PreciseShardingValue<Integer> shardingValue) {
+        for (String each : availableTargetNames) {
+            if (each.endsWith(shardingValue.getValue() / 2 % 2 + "")) {
+                return each;
+            }
+        }
+        throw new UnsupportedOperationException();
+    }
+}
