e41e8e2ff1fb3066e7419ab269941896651000cb
WEIZIBIN <1999597@qq.com>
2018-10-24 20:12:27 +0800

    save

--- thinking-in-java/src/main/java/github/weizibin/datastructure/AVLTree.java
+++ thinking-in-java/src/main/java/github/weizibin/datastructure/AVLTree.java
@@ -1,6 +1,56 @@
 package github.weizibin.datastructure;
 
-// TODO: 2018/10/24  
-public class AVLTree {
-}
+import github.weizibin.datastructure.util.TreeUtils;
 
+// TODO: 2018/10/24
+public class AVLTree<T extends Comparable> {
+
+    public TreeNode<T> root;
+
+    public void insert(T content) {
+        TreeNode<T> newNode = new TreeNode<>(null, null, content);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+
+        TreeNode<T> node = root;
+        while (node != null) {
+            boolean less = content.compareTo(node.content) < 0;
+            TreeNode<T> next = less ? node.left : node.right;
+            if (next == null) {
+                if (less) {
+                    node.left = newNode;
+                } else {
+                    node.right = newNode;
+                }
+            }
+            node = next;
+        }
+
+    }
+
+    private static TreeNode leftRotate(TreeNode node) {
+        TreeNode root = node.right;
+        root.left = node;
+        node.right = null;
+        return root;
+    }
+
+    private static TreeNode rightRotate(TreeNode node) {
+        TreeNode root = node.left;
+        root.right = node;
+        node.left = null;
+        return root;
+    }
+
+    public static void main(String[] args) {
+        AVLTree<Integer> avlTree = new AVLTree<>();
+        avlTree.insert(1);
+        avlTree.insert(2);
+        avlTree.insert(3);
+        avlTree.insert(4);
+        TreeUtils.print(avlTree.root);
+    }
+
+}
--- thinking-in-java/src/main/java/github/weizibin/datastructure/TreeNode.java
+++ thinking-in-java/src/main/java/github/weizibin/datastructure/TreeNode.java
@@ -1,18 +1,19 @@
 package github.weizibin.datastructure;
 
-// TODO: 2018/10/24  
+// TODO: 2018/10/24
 public class TreeNode<T> {
-    public TreeNode left;
-    public TreeNode right;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
     public T content;
 
-    public TreeNode() {
-
-    }
-
-    public TreeNode(TreeNode left, TreeNode right, T content) {
+    public TreeNode(TreeNode<T> left, TreeNode<T> right, T content) {
         this.left = left;
         this.right = right;
         this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return content.toString();
     }
 }
