84d28a457720af252ac00416186b11017f541f60
WEIZIBIN <danielweiup@gmail.com>
2018-11-20 17:35:53 +0800

    optimizing ConstructTreeFromGivenOrder.java

--- thinking-in-java/src/main/java/github/weizibin/datastructure/ConstructTreeFromGivenOrder.java
+++ thinking-in-java/src/main/java/github/weizibin/datastructure/ConstructTreeFromGivenOrder.java
@@ -4,52 +4,34 @@ import github.weizibin.datastructure.util.TreeUtils;
 
 public class ConstructTreeFromGivenOrder {
 
-    public static <T> TreeNode<T> constructFromPreorderAndInorder(T[] preorder, T[] inorder) {
-        if (preorder.length == 0) {
+    public static <T> TreeNode<T> constructFromPreorderAndInorder(int preStart, int inStart, int inEnd, T[] preorder, T[] inorder) {
+        if (inStart > inEnd) {
             return null;
         }
-        T rootVal = preorder[0];
-        TreeNode<T> root = new TreeNode<>(null, null, rootVal);
-        if (preorder.length == 1) {
-            return root;
-        }
 
+        T rootVal = preorder[preStart];
+        TreeNode<T> root = new TreeNode<>(null, null, rootVal);
         int inorderIndex = -1;
-        for (int i = 0; i < inorder.length; i++) {
+        for (int i = inStart; i < inEnd + 1; i++) {
             if (rootVal.equals(inorder[i])) {
                 inorderIndex = i;
+                break;
             }
         }
 
-        int leftTreeSize = inorderIndex;
-        int rightTreeSize = preorder.length - inorderIndex - 1;
-
-        T[] newLeftPreorder = (T[]) new Object[leftTreeSize];
-        T[] newRightPreorder = (T[]) new Object[rightTreeSize];
-        T[] newLeftInorder = (T[]) new Object[leftTreeSize];
-        T[] newRightInorder = (T[]) new Object[rightTreeSize];
-
-        System.arraycopy(preorder, 1, newLeftPreorder, 0, leftTreeSize);
-        System.arraycopy(preorder, inorderIndex + 1, newRightPreorder, 0, rightTreeSize);
-        System.arraycopy(inorder, 0, newLeftInorder, 0, leftTreeSize);
-        System.arraycopy(inorder, inorderIndex + 1, newRightInorder, 0, rightTreeSize);
-
-        root.left = constructFromPreorderAndInorder(newLeftPreorder, newLeftInorder);
-        root.right = constructFromPreorderAndInorder(newRightPreorder, newRightInorder);
+        root.left = constructFromPreorderAndInorder(preStart + 1, inStart, inorderIndex - 1, preorder, inorder);
+        root.right = constructFromPreorderAndInorder(preStart + 1 + inorderIndex - inStart, inorderIndex + 1, inEnd, preorder, inorder);
 
         return root;
     }
 
-    public static <T> TreeNode<T> constructFromInorderAndPostorder(T[] inorder, T[] postorder) {
-        if (postorder.length == 0) {
+    public static <T> TreeNode<T> constructFromInorderAndPostorder(int postEnd, int inStart, int inEnd, T[] inorder, T[] postorder) {
+        if (inStart > inEnd) {
             return null;
         }
-        T rootVal = postorder[postorder.length - 1];
-        TreeNode<T> root = new TreeNode<>(null, null, rootVal);
-        if (postorder.length == 1) {
-            return root;
-        }
 
+        T rootVal = postorder[postEnd];
+        TreeNode<T> root = new TreeNode<>(null, null, rootVal);
         int inorderIndex = -1;
         for (int i = 0; i < inorder.length; i++) {
             if (rootVal.equals(inorder[i])) {
@@ -57,31 +39,19 @@ public class ConstructTreeFromGivenOrder {
             }
         }
 
-        int leftTreeSize = inorderIndex;
-        int rightTreeSize = postorder.length - inorderIndex - 1;
-
-        T[] newLeftPostorder = (T[]) new Object[leftTreeSize];
-        T[] newRightPostorder = (T[]) new Object[rightTreeSize];
-        T[] newLeftInorder = (T[]) new Object[leftTreeSize];
-        T[] newRightInorder = (T[]) new Object[rightTreeSize];
-
-        System.arraycopy(postorder, 0, newLeftPostorder, 0, leftTreeSize);
-        System.arraycopy(postorder, inorderIndex, newRightPostorder, 0, rightTreeSize);
-        System.arraycopy(inorder, 0, newLeftInorder, 0, leftTreeSize);
-        System.arraycopy(inorder, inorderIndex + 1, newRightInorder, 0, rightTreeSize);
-
-        root.left = constructFromInorderAndPostorder(newLeftInorder, newLeftPostorder);
-        root.right = constructFromInorderAndPostorder(newRightInorder, newRightPostorder);
+        root.left = constructFromInorderAndPostorder(postEnd - 1 + inorderIndex - inEnd, inStart, inorderIndex - 1, inorder, postorder );
+        root.right = constructFromInorderAndPostorder(postEnd - 1, inorderIndex + 1, inEnd,inorder, postorder);
 
         return root;
+
     }
 
     public static void main(String[] args) {
-        String[] preorder = {"A","B","D","E","C","F"};
-        String[] inorder = {"D","B","E","A","F","C"};
-        String[] postorder = {"D","E","B","F","C","A"};
-        TreeNode root1 = constructFromPreorderAndInorder(preorder, inorder);
-        TreeNode root2 = constructFromInorderAndPostorder(inorder, postorder);
+        String[] preorder = {"A", "B", "D", "E", "C", "F"};
+        String[] inorder = {"D", "B", "E", "A", "F", "C"};
+        String[] postorder = {"D", "E", "B", "F", "C", "A"};
+        TreeNode root1 = constructFromPreorderAndInorder(0, 0, inorder.length - 1, preorder, inorder);
+        TreeNode root2 = constructFromInorderAndPostorder(postorder.length - 1,0, inorder.length - 1,inorder, postorder);
         TreeUtils.print(root1);
         TreeUtils.print(root2);
     }
